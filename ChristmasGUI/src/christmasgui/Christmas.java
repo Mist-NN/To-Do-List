f16aec50235c2cfd4648edbaee2a2244b5ae8ebd
Mist-NN <2407387475@qq.com>
2024-12-23 23:16:12 +0800

    Add files via upload

---
+++ ChristmasGUI/src/christmasgui/Christmas.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package christmasgui;
+
+/**
+ *
+ * @author Yi Wu
+ */
+public class Christmas {
+    private String name;
+    private String url;
+    private String idea;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setUrl(String url){
+        this.url = url;
+    }
+    public void setIdea(String idea){
+        this.idea = idea;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getUrl(){
+        return url;
+    }
+    public String getIdea(){
+        return idea;
+    }
+}
